016b192176c0d2b98ade177b974b9f84efbc2412
Koray Guney <koray.guney@hotmail.com>
2022-12-21 18:47:28 +0300

    Initial commit

---
+++ src/main/java/com/sahibinden/helloworld/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.sahibinden.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String serveWebPage() {
+        return "index";
+    }
+}
